5ee4a834e233be30800b1334dd8c4c49d0769c95
ereibor <etunere@gmail.com>
2026-04-02 18:41:56 +0100

    refactor Button component to remove routing logic and simplify onClick handling

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useRouter } from "next/navigation";
 import React, { ReactNode } from "react";
 
 interface ButtonProps {
@@ -7,8 +6,6 @@ interface ButtonProps {
   color?: string;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
-  href?: string;
-
   leftIcon?: ReactNode;
   rightIcon?: ReactNode;
 }
@@ -18,26 +15,13 @@ const Button = ({
   color = "bg-blue-500 hover:bg-blue-600",
   type = "button",
   onClick,
-  href,
   leftIcon,
   rightIcon,
 }: ButtonProps) => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    if (href) {
-      router.push(href);
-    }
-
-    if (onClick) {
-      onClick();
-    }
-  };
-
   return (
     <button
       type={type}
-      onClick={handleClick}
+      onClick={onClick}
       className={`flex items-center justify-center gap-2 px-5 py-2 rounded-lg text-white text-sm font-medium transition-colors shadow-sm cursor-pointer ${color}`}
     >
       {leftIcon && <span className="flex items-center">{leftIcon}</span>}
